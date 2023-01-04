effacfe29ecca682bf9e626610c7c0cfd135015c
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-01-04 13:58:58 -0500

    acabe mobile

---
+++ src/app.js
@@ -0,0 +1,7 @@
+let buttomImg = document.querySelector('.div_img')
+let containNav = document.querySelector('.contain_nav')
+
+buttomImg.addEventListener('click',function(){
+    containNav.classList.toggle('contain_nav_hide')
+    console.log('click');
+})
