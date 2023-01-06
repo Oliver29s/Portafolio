c3fedab58d4c9a9ee2733dab8cf0c5cf2431ec25
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-01-06 11:18:50 -0500

    tratare de agregar white mode

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-let buttomImg = document.querySelector('.div_img')
+let buttomImg = document.querySelector('.buttom_img')
 let containNav = document.querySelector('.contain_nav')
 
 buttomImg.addEventListener('click',function(){
     containNav.classList.toggle('contain_nav_hide')
     
 })
+
+
 
